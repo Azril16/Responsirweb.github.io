ffba8fad34e258b37cb883746a503d1d3942521f
Azril16 <azril.evan1629@gmail.com>
2022-07-17 00:25:29 +0700

    responsi rweb

---
+++ application/views/index.php
@@ -0,0 +1,487 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<title>RESPONSI RWEB</title>
+	<link rel="stylesheet" href="assets/css/loop.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/slick/slick.css">
+	<link rel="stylesheet" href="assets/slick/slick-theme.css">
+	<link rel="stylesheet" href="assets/fontawesome-5.5/css/all.min.css">
+	<link rel="stylesheet" href="assets/magnific-popup/magnific-popup.css">
+
+</head>
+
+<body>
+	<!-- AWAL HERO -->
+	<section id="infinite" class="text-white tm-font-big tm-parallax">
+
+		<!-- AWAL NAV BAR -->
+		<nav class="navbar navbar-expand-md tm-navbar" id="tmNav">
+			<div class="container">
+				<div class="tm-next">
+					<a href="#infinite" class="navbar-brand">PARIWISATA YOGYAKARTA</a>
+				</div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<i class="fas fa-bars navbar-toggler-icon"></i>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link tm-nav-link" href="#infinite">Beranda</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link tm-nav-link" href="#whatwedo">Ensiklopedia</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link tm-nav-link" href="#testimonials">Tour Guide</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link tm-nav-link" href="#gallery">Galeri</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link tm-nav-link" href="#contact">Kontak</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<!-- AKHIR NAV BAR-->
+
+		<div class="text-center tm-hero-text-container">
+			<div class="tm-hero-text-container-inner">
+				<h2 class="tm-hero-title">PARIWISATA YOGYAKARTA</h2>
+				<p class="tm-hero-subtitle">
+					Yogyakarta Berhati Nyaman
+					<br>Menjelajah Sejarah Di Tanah Jawa
+				</p>
+			</div>
+		</div>
+
+		<div class="tm-next tm-intro-next">
+			<a href="#whatwedo" class="text-center tm-down-arrow-link">
+				<i class="fas fa-2x fa-arrow-down tm-down-arrow"></i>
+			</a>
+		</div>
+	</section>
+	<!-- AKHIR HERO -->
+
+	<!-- AWAL BERANDA & ENSIKLOPEDIA -->
+	<section id="whatwedo" class="tm-section-pad-top">
+		<div class="container">
+			<div class="row tm-content-box">
+				<!-- JUDUL AWAL WEBSITE -->
+				<div class="col-lg-12 col-xl-12">
+					<div class="tm-intro-text-container">
+						<h2 class="tm-text-primary mb-4 tm-section-title">Yogyakarta</h2>
+						<p class="mb-4 tm-intro-text">
+							Daerah Istimewa Yogyakarta terletak di bagian selatan pulau Jawa, berbatasan dengan provinsi Jawa Tengah. Awalnya, Yogyakarta ialah kerajaan yang meliputi wilayah Kasultanan Ngayogyakarta hadiningrat dan Kadipaten Pakualaman, ditambah bekas wilayah Kasunan Surakarta Hadiningrat dan Praja Mangkunagaran</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row tm-content-box">
+				<!-- AWAL ROW PENGUNJUNG -->
+				<div class="col-lg-1">
+					<i class="far fa-3x fa-chart-bar text-center tm-icon"></i>
+				</div>
+				<div class="col-lg-5">
+					<div class="tm-intro-text-container">
+						<h2 class="tm-text-primary mb-4">Pengunjung</h2>
+						<p class="mb-4 tm-intro-text">
+							Pengunjung wisata yang berasal dari dalam negeri maupun luar negeri</p>
+					</div>
+				</div>
+				<!-- AKHIR ROW PENGUNJUNG -->
+				<!-- AWAL ROW RESPON CEPAT-->
+				<div class="col-lg-1">
+					<i class="far fa-3x fa-comment-alt text-center tm-icon"></i>
+				</div>
+				<div class="col-lg-5">
+					<div class="tm-intro-text-container">
+						<h2 class="tm-text-primary mb-4">Respon Cepat</h2>
+						<p class="mb-4 tm-intro-text">
+							Tour Guide kami yang siap melayani kunjungan wisata 24 jam selama 7 hari</p>
+					</div>
+				</div>
+				<!-- AKHIR ROW RESPON CEPAT-->
+			</div>
+		</div>
+	</section>
+	<!-- AKHIR BERANDA & ENSIKLOPEDIA -->
+
+	<!-- AWAL TOUR GUIDE -->
+	<section id="testimonials" class="tm-section-pad-top tm-parallax-2">
+		<div class="container tm-testimonials-content">
+			<div class="row">
+				<div class="col-lg-12 tm-content-box">
+					<h2 class="text-white text-center mb-4 tm-section-title">TOUR GUIDE</h2>
+					<p class="mx-auto tm-section-desc text-center">
+						Kami menyediakan Tour Guide yang akan memandu setiap perjalanan anda di kota Yogyakarta dengan nyaman, aman, serta terpercaya
+					</p>
+					<div class="mx-auto tm-gallery-container tm-gallery-container-2">
+						<div class="tm-testimonials-carousel">
+							<figure class="tm-testimonial-item">
+								<img src="assets/img/tourguide_1.jpg" alt="Image" class="img-fluid mx-auto">
+								<blockquote>Wanita penyuka petualangan, fasih berbahasa inggris serta mengetahui hidden gems yang terletak di Yogyakarta</blockquote>
+								<figcaption>Wulan Ningsih</figcaption>
+							</figure>
+
+							<figure class="tm-testimonial-item">
+								<img src="assets/img/tourguide_2.png" alt="Image" class="img-fluid mx-auto">
+								<blockquote>Lelaki muda yang gemar membaca, ramah dan juga humble membuat perjalanan anda berkeliling Yogyakarta berkesan</blockquote>
+								<figcaption>Cahya Pustaka</figcaption>
+							</figure>
+
+							<figure class="tm-testimonial-item">
+								<img src="assets/img/tourguide_3.png" alt="Image" class="img-fluid mx-auto">
+								<blockquote>Bertanggung Jawab, Optimis, serta lucu yang mengetahui tempat-tempat extreme di Yogyakarta</blockquote>
+								<figcaption>Tika Pustika</figcaption>
+							</figure>
+
+							<figure class="tm-testimonial-item">
+								<img src="assets/img/tourguide_4.png" alt="Image" class="img-fluid mx-auto">
+								<blockquote>Pria berbadan tegap, dengan kulit berwarna putih yang sangat menyukai tantangan.</blockquote>
+								<figcaption>Hafidzh Naufal</figcaption>
+							</figure>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="tm-bg-overlay"></div>
+	</section>
+	<!-- AKHIR TOUR GUIDE -->
+
+	<!-- AWAL GALERI -->
+	<section id="gallery" class="tm-section-pad-top">
+		<div class="container tm-container-gallery">
+			<div class="row">
+				<div class="text-center col-12">
+					<h2 class="tm-text-primary tm-section-title mb-4">Galeri</h2>
+					<p class="mx-auto tm-section-desc">
+						Berisikan beberapa foto obyek wisata yang wajib dikunjungi di Yogyakarta
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<div class="mx-auto tm-gallery-container">
+						<div class="grid tm-gallery">
+							<a href="assets/img/Gambar_1_Tugu_Yogya.png">
+								<figure class="effect-honey tm-gallery-item">
+									<img src="assets/img/Gambar_1_Tugu_Yogya.png" alt="Image 1" class="img-fluid">
+									<figcaption>
+										<h2><i>TUGU <span>YOGYAKARTA</span></i></h2>
+									</figcaption>
+								</figure>
+							</a>
+							<a href="assets/img/Gambar_2_Candi_Prambanan.png">
+								<figure class="effect-honey tm-gallery-item">
+									<img src="assets/img/Gambar_2_Candi_Prambanan.png" alt="Image 2" class="img-fluid">
+									<figcaption>
+										<h2><i>CANDI <span>PRAMBANAN</span></i></h2>
+									</figcaption>
+								</figure>
+							</a>
+							<a href="assets/img/Gambar_3_Merapi.png">
+								<figure class="effect-honey tm-gallery-item">
+									<img src="assets/img/Gambar_3_Merapi.png" alt="Image 5" class="img-fluid">
+									<figcaption>
+										<h2><i>GUNUNG <span>MERAPI</span></i></h2>
+									</figcaption>
+								</figure>
+							</a>
+							<a href="assets/img/Gambar_4_Parangtritis.png">
+								<figure class="effect-honey tm-gallery-item">
+									<img src="assets/img/Gambar_4_Parangtritis.png" alt="Image 4" class="img-fluid">
+									<figcaption>
+										<h2><i>PANTAI <span>PARANGTRITIS</span></i></h2>
+									</figcaption>
+								</figure>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- AKHIR GALERI -->
+
+	<!-- AWAL KONTAK -->
+	<section id="contact" class="tm-section-pad-top tm-parallax-2">
+
+		<div class="container tm-container-contact">
+
+			<div class="row">
+
+				<div class="text-center col-12">
+					<h2 class="tm-section-title mb-4">Kontak Kami</h2>
+					<p class="mb-5">
+						Untuk informasi lebih lanjut mengenai syarat & ketentuan dari pelayanan kami, silahkan hubungi dibawah ini
+					</p><br>
+				</div>
+
+				<div class="col-sm-12 col-md-6">
+					<form action="" method="get">
+						<input id="name" name="name" type="text" placeholder="Nama Anda" class="tm-input" required />
+						<input id="email" name="email" type="email" placeholder="Email Anda" class="tm-input" required />
+						<textarea id="message" name="message" rows="8" placeholder="Pesan" class="tm-input" required></textarea>
+						<button type="submit" class="btn tm-btn-submit">Kirim</button>
+					</form>
+				</div>
+
+				<div class="col-sm-12 col-md-6">
+
+					<div class="contact-item">
+						<a rel="nofollow" href="https://www.instagram.com/informatika.uad/" class="item-link">
+							<i class="far fa-2x fa-comment mr-4"></i>
+							<span class="mb-0">Chat Online</span>
+						</a>
+					</div>
+
+					<div class="contact-item">
+						<a rel="nofollow" href="info@uad.ac.id" class="item-link">
+							<i class="far fa-2x fa-envelope mr-4"></i>
+							<span class="mb-0">yogyakarta@companygov.com</span>
+						</a>
+					</div>
+
+					<div class="contact-item">
+						<a rel="nofollow" href="https://goo.gl/maps/mhF3r7NXx1ZMKQ4q8" class="item-link">
+							<i class="fas fa-2x fa-map-marker-alt mr-4"></i>
+							<span class="mb-0">Lokasi Kami</span>
+						</a>
+					</div>
+
+					<div class="contact-item">
+						<a rel="nofollow" href="https://wa.link/sd2j8r" class="item-link">
+							<i class="fas fa-2x fa-phone-square mr-4"></i>
+							<span class="mb-0">0821-9955-3034</span>
+						</a>
+					</div>
+
+					<div class="contact-item">&nbsp;</div>
+
+				</div>
+
+
+			</div>
+			<!-- row ending -->
+
+		</div>
+
+		<footer class="text-center small tm-footer">
+			<p class="mb-0">
+				Copyright &copy; 2022 REKAYASA WEB
+
+				. Design & Build By . </p>
+			<p>Chandra Surya Negarawan – 1900018101 (Selasa, 16.30)</p>
+			<p>Andre Pandia – 1900018180 (Kamis, 15.00)</p>
+			<p>Muhammad Azril Revanza F – 1900018182 (Kamis, 15.00)</p>
+		</footer>
+
+	</section>
+	<!-- AKHIR KONTAK -->
+
+	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="slick/slick.min.js"></script>
+	<script src="magnific-popup/jquery.magnific-popup.min.js"></script>
+	<script src="js/easing.min.js"></script>
+	<script src="js/jquery.singlePageNav.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script>
+		function getOffSet() {
+			var _offset = 450;
+			var windowHeight = window.innerHeight;
+
+			if (windowHeight > 500) {
+				_offset = 400;
+			}
+			if (windowHeight > 680) {
+				_offset = 300
+			}
+			if (windowHeight > 830) {
+				_offset = 210;
+			}
+
+			return _offset;
+		}
+
+		function setParallaxPosition($doc, multiplier, $object) {
+			var offset = getOffSet();
+			var from_top = $doc.scrollTop(),
+				bg_css = 'center ' + (multiplier * from_top - offset) + 'px';
+			$object.css({
+				"background-position": bg_css
+			});
+		}
+
+
+		var background_image_parallax = function($object, multiplier, forceSet) {
+			multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+			multiplier = 1 - multiplier;
+			var $doc = $(document);
+			// $object.css({"background-attatchment" : "fixed"});
+
+			if (forceSet) {
+				setParallaxPosition($doc, multiplier, $object);
+			} else {
+				$(window).scroll(function() {
+					setParallaxPosition($doc, multiplier, $object);
+				});
+			}
+		};
+
+		var background_image_parallax_2 = function($object, multiplier) {
+			multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+			multiplier = 1 - multiplier;
+			var $doc = $(document);
+			$object.css({
+				"background-attachment": "fixed"
+			});
+
+			$(window).scroll(function() {
+				if ($(window).width() > 768) {
+					var firstTop = $object.offset().top,
+						pos = $(window).scrollTop(),
+						yPos = Math.round((multiplier * (firstTop - pos)) - 186);
+
+					var bg_css = 'center ' + yPos + 'px';
+
+					$object.css({
+						"background-position": bg_css
+					});
+				} else {
+					$object.css({
+						"background-position": "center"
+					});
+				}
+			});
+		};
+
+		$(function() {
+			// Hero Section - Background Parallax
+			background_image_parallax($(".tm-parallax"), 0.30, false);
+			background_image_parallax_2($("#contact"), 0.80);
+			background_image_parallax_2($("#testimonials"), 0.80);
+
+			// Handle window resize
+			window.addEventListener('resize', function() {
+				background_image_parallax($(".tm-parallax"), 0.30, true);
+			}, true);
+
+			// Detect window scroll and update navbar
+			$(window).scroll(function(e) {
+				if ($(document).scrollTop() > 120) {
+					$('.tm-navbar').addClass("scroll");
+				} else {
+					$('.tm-navbar').removeClass("scroll");
+				}
+			});
+
+			// Close mobile menu after click 
+			$('#tmNav a').on('click', function() {
+				$('.navbar-collapse').removeClass('show');
+			})
+
+			// Scroll to corresponding section with animation
+			$('#tmNav').singlePageNav({
+				'easing': 'easeInOutExpo',
+				'speed': 600
+			});
+
+			// Add smooth scrolling to all links
+			// https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
+			$("a").on('click', function(event) {
+				if (this.hash !== "") {
+					event.preventDefault();
+					var hash = this.hash;
+
+					$('html, body').animate({
+						scrollTop: $(hash).offset().top
+					}, 600, 'easeInOutExpo', function() {
+						window.location.hash = hash;
+					});
+				} // End if
+			});
+
+			// Pop up
+			$('.tm-gallery').magnificPopup({
+				delegate: 'a',
+				type: 'image',
+				gallery: {
+					enabled: true
+				}
+			});
+
+			$('.tm-testimonials-carousel').slick({
+				dots: true,
+				prevArrow: false,
+				nextArrow: false,
+				infinite: false,
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				responsive: [{
+						breakpoint: 992,
+						settings: {
+							slidesToShow: 2
+						}
+					},
+					{
+						breakpoint: 768,
+						settings: {
+							slidesToShow: 2
+						}
+					},
+					{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1
+						}
+					}
+				]
+			});
+
+			// Gallery
+			$('.tm-gallery').slick({
+				dots: true,
+				infinite: false,
+				slidesToShow: 5,
+				slidesToScroll: 2,
+				responsive: [{
+						breakpoint: 1199,
+						settings: {
+							slidesToShow: 4,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 991,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 767,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 1
+						}
+					},
+					{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
+		});
+	</script>
+</body>
+
+</html>
